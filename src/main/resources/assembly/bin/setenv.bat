45588fb359c5e02fc30852a060d835c838ac541a
Jeremy Landis <jeremylandis@hotmail.com>
2019-05-05 14:17:25 -0400

    [migrate] Move resources into main

--- src/main/resources/assembly/bin/setenv.bat
+++ src/main/resources/assembly/bin/setenv.bat
@@ -1,7 +1,7 @@
 @REM
 @REM Tomcat-Slf4j-Logback (https://github.com/tomcat-slf4j-logback/tomcat-slf4j-logback/)
 @REM
-@REM Copyright (c) 2010-2017 Tomcat-Slf4j-Logback.
+@REM Copyright (c) 2010-2019 Tomcat-Slf4j-Logback.
 @REM
 @REM All rights reserved. This program and the accompanying materials are made available under the
 @REM terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available
