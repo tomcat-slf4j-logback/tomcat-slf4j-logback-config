ccaa5cc0b73c3bb3e10bb01aded482975a1209bb
Jeremy Landis <jeremylandis@hotmail.com>
2026-02-07 20:20:50 -0500

    [license] Use spdx as license header for current epl-1.0

--- src/main/resources/assembly/bin/setenv.bat
+++ src/main/resources/assembly/bin/setenv.bat
@@ -1,13 +1,8 @@
 @REM
-@REM Tomcat-Slf4j-Logback (https://github.com/tomcat-slf4j-logback/tomcat-slf4j-logback-config/)
+@REM SPDX-License-Identifier: EPL-1.0
+@REM See LICENSE file for details.
 @REM
-@REM Copyright (c) 2010-2019 Tomcat-Slf4j-Logback.
-@REM
-@REM All rights reserved. This program and the accompanying materials are made available under the
-@REM terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available
-@REM at https://www.eclipse.org/legal/epl-v10.html.
-@REM
-@REM Contributors: Tomcat-Slf4j-Logback Team.
+@REM Copyright 2010-2026 tomcat-slf4j-logback
 @REM
 
 set JAVA_OPTS=%JAVA_OPTS% -Djuli-logback.configurationFile="%CATALINA_HOME%\conf\logback.xml"
